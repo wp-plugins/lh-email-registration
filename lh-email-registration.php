c771fe8ad430e3274d00dd6e82bd359980a509d0
shawfactor <shawfactor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-22 11:41:22 +0000

    version 2.0
    git-svn-id: https://plugins.svn.wordpress.org/lh-email-registration/trunk@1227967 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lh-email-registration.php
+++ lh-email-registration.php
@@ -3,7 +3,7 @@
 Plugin Name: LH Email Registration
 Plugin URI: http://wordpress.org/plugins/lh-email-registration/
 Description: Streamlines user registration in the backend by removing redundant fields and replaces usernames with email adresses
-Version: 1.1
+Version: 2.0
 Author: Peter Shaw
 Author URI: http://shawfactor.com
 License: GPLv2 or later
@@ -26,13 +26,26 @@ License: GPLv2 or later
 
 class LH_email_registration_plugin {
 
+var $opt_name = "lh_email_registration-options";
+var $hidden_field_name = 'lh_email_registration-submit_hidden';
+var $use_email_field_name = 'lh_email_registration-use_email';
+var $remove_url_field_name = 'lh_email_registration-remove_url';
+var $password_email_field_name = 'lh_email_registration-password_email';
+var $options;
+var $filename;
+
 public function reconfigure_fields_by_js() {
 
 ?>
-<script>jQuery(document).ready(function(){jQuery('#url').parents('tr').remove();});
+<script>
+
+jQuery(document).ready(function(){
 
 
-function lh_users_generatePassword() {
+<?php  if ($this->options[$this->use_email_field_name] == 1){?>
+
+
+function lh_users_generateRandom() {
     var length = 8,
         charset = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
         retVal = "";
@@ -42,36 +55,31 @@ function lh_users_generatePassword() {
     return retVal;
 }
 
-
-
-jQuery(document).ready(function(){
-
-password = lh_users_generatePassword();
-
-jQuery('#pass1').parents('tr').remove();
-
-jQuery('#pass2').parents('tr').remove();
-
-string = '<input type="hidden" name="pass1" value="' + password + '" />';
-
-string2 = '<input type="hidden" name="pass2" value="' + password + '" />';
-
-jQuery('#createuser').append(string);
-
-jQuery('#createuser').append(string2);
-
-
-
-jQuery('#pass2').parents('tr').remove();
+password = lh_users_generateRandom();
 
 jQuery('#user_login').val(password);
 
 jQuery('#user_login').parents('tr').hide();
 
 
-jQuery('#send_password').parents('tr').html('<th scope="row"><label for="send_password">Send Password?</label></th><td><label for="send_password"><input type="checkbox" name="send_password" id="send_password" value="1"  /> Send the new user a randomly generated password by email.</label></td>');
 
 
+<?php }  ?>
+
+
+
+
+<?php  if ($this->options[$this->remove_url_field_name] == 1){?>jQuery('#url').parents('tr').remove();   <?php }  ?>
+
+
+<?php  if ($this->options[$this->password_email_field_name] == 1){ 
+?>
+
+jQuery('#pass1').parents('tr').html('<th scope="row"><label for="pass1">Password<span class="description hide-if-js">(required)</span></label></th><td><input class="hidden" value=" " /><!-- #24364 workaround --><button type="button" class="button button-secondary wp-generate-pw hide-if-no-js">Show password</button><div class="wp-pwd hide-if-js"><span class="password-input-wrapper"><input type="password" name="pass1" id="pass1" class="regular-text" autocomplete="off" data-reveal="1" data-pw="*kxDlyekB2NYwEzrU0EUYjAR" aria-describedby="pass-strength-result" /></span><button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Hide password"><span class="dashicons dashicons-hidden"></span><span class="text">Hide</span></button><button type="button" class="button button-secondary wp-cancel-pw hide-if-no-js" data-toggle="0" aria-label="Cancel password change"><span class="text">Cancel</span></button><div style="display:none" id="pass-strength-result" aria-live="polite"></div></div><p><span class="description">Send the new user the generated password by email? <input type="checkbox" name="send_password" id="send_password" value="1"  /></span></p></td>');
+
+
+
+<?php }  ?>
 
 });
 
@@ -88,10 +96,13 @@ public function override_usernames_on_save( $user_id ) {
 
 global $wpdb;
 
+if ($this->options[$this->use_email_field_name] == 1){
+
 $sql = "update ".$wpdb->prefix."users set user_login = user_email where ID = '".$user_id."'";
 
 $result = $wpdb->get_results($sql);
 
+}
 
 }
 
@@ -109,8 +120,7 @@ remove_all_actions( 'user_new_form');
 public function wp_redirect_after_user_new( $location ){
     global $pagenow;
 
-    if( is_admin() && 'user-new.php' == $pagenow )
-    {
+    if( is_admin() && 'user-new.php' == $pagenow ) {
         $user_details = get_user_by( 'email', $_REQUEST[ 'email' ] );
         $user_id = $user_details->ID;
 
@@ -123,13 +133,132 @@ public function wp_redirect_after_user_new( $location ){
 
 
 
+function plugin_menu() {
+add_options_page('LH Email Registration', 'LH Email Registration', 'manage_options', $this->filename, array($this,"plugin_options"));
+
+}
+
+function plugin_options() {
+
+if (!current_user_can('manage_options')){
+
+wp_die( __('You do not have sufficient permissions to access this page.') );
+
+}
+
+if( isset($_POST[ $this->hidden_field_name ]) && $_POST[ $this->hidden_field_name ] == 'Y' ) {
+
+        // Read their posted value
+
+
+if (($_POST[$this->use_email_field_name] == "0") || ($_POST[$this->use_email_field_name] == "1")){
+$options[$this->use_email_field_name] = $_POST[ $this->use_email_field_name ];
+}
+
+if (($_POST[$this->remove_url_field_name] == "0") || ($_POST[$this->remove_url_field_name] == "1")){
+$options[$this->remove_url_field_name] = $_POST[ $this->remove_url_field_name ];
+}
+
+if (($_POST[$this->password_email_field_name] == "0") || ($_POST[$this->password_email_field_name] == "1")){
+$options[$this->password_email_field_name] = $_POST[ $this->password_email_field_name ];
+}
+
+
+if (update_site_option( $this->opt_name, $options )){
+
+$this->options = get_site_option($this->opt_name);
+
+?>
+<div class="updated"><p><strong><?php _e('User registration settings saved', 'menu-test' ); ?></strong></p></div>
+<?php
+
+
+}
+
+
+}
+
+
+  // Now display the settings editing screen
+
+    echo '<div class="wrap">';
+
+    // header
+
+echo "<h1>" . __('LH Email Registration', 'menu-test' ) . "</h1>";
+
+    // settings form
+    
+    ?>
+
+<form name="lh_email_registration-backend_form" method="post" action="">
+<input type="hidden" name="<?php echo $this->hidden_field_name; ?>" value="Y" />
+
+<p><label for="<?php echo $this->use_email_field_name; ?>"><?php _e("Use only emails:", 'menu-test' ); ?></label>
+<select name="<?php echo $this->use_email_field_name; ?>" id="<?php echo $this->use_email_field_name; ?>">
+<option value="1" <?php  if ($this->options[$this->use_email_field_name] == 1){ echo 'selected="selected"'; }  ?>>Yes</option>
+<option value="0" <?php  if ($this->options[$this->use_email_field_name] == 0){ echo 'selected="selected"';}  ?>>No</option>
+</select> - Set this to yes if you want your users usernames to be automatically set the same as their email
+</p>
+
+<p><label for="<?php echo $this->remove_url_field_name; ?>"><?php _e("Remove URL field:", 'menu-test' ); ?></label>
+<select name="<?php echo $this->remove_url_field_name; ?>" id="<?php echo $this->remove_url_field_name; ?>">
+<option value="1" <?php  if ($this->options[$this->remove_url_field_name] == 1){ echo 'selected="selected"'; }  ?>>Yes</option>
+<option value="0" <?php  if ($this->options[$this->remove_url_field_name] == 0){ echo 'selected="selected"';}  ?>>No</option>
+</select> - Set this to yes to hide the url field in the new user set up
+</p>
+
+<p><label for="<?php echo $this->password_email_field_name; ?>"><?php _e("Make password email optional:", 'menu-test' ); ?></label>
+<select name="<?php echo $this->password_email_field_name; ?>" id="<?php echo $this->password_email_field_name; ?>">
+<option value="1" <?php  if ($this->options[$this->password_email_field_name] == 1){ echo 'selected="selected"'; }  ?>>Yes</option>
+<option value="0" <?php  if ($this->options[$this->password_email_field_name] == 0){ echo 'selected="selected"';}  ?>>No</option>
+</select> - Set this to yes if you want too make sending the email to new users optional
+</p>
+
+
+<p class="submit">
+<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+</p>
+
+</form>
+
+
+
+</div>
+
+<?php
+
+}
+
+// add a settings link next to deactive / edit
+public function add_settings_link( $links, $file ) {
+
+	if( $file == $this->filename ){
+		$links[] = '<a href="'. admin_url( 'options-general.php?page=' ).$this->filename.'">Settings</a>';
+	}
+	return $links;
+}
+
+public function return_password_email() {
+
+return $this->options[$this->password_email_field_name];
+
+}
+
+
 function __construct() {
 
+$this->options = get_site_option($this->opt_name);
+$this->filename = plugin_basename( __FILE__ );
+
+
+add_action('admin_menu', array($this,"plugin_menu"));
 add_action( 'wp_loaded', array($this,"run_wp_loaded"));
 add_action('admin_head-user-new.php',array($this,"reconfigure_fields_by_js"));
 add_action('user_register', array($this,"override_usernames_on_save"), 10, 1 );
 add_action('profile_update', array($this,"override_usernames_on_save"), 10, 2 );
 add_filter('wp_redirect', array($this,"wp_redirect_after_user_new"), 1, 1 );
+add_filter('plugin_action_links', array($this,"add_settings_link"), 10, 2);
 
 
 }
@@ -140,6 +269,93 @@ add_filter('wp_redirect', array($this,"wp_redirect_after_user_new"), 1, 1 );
 
 $lh_email_registration = new LH_email_registration_plugin();
 
+
+function lh_email_registration_return_password_email(){
+
+$lh_email_registration_again = new LH_email_registration_plugin();
+
+$email = $lh_email_registration_again->return_password_email();
+
+if ($email == 0){
+
+return true;
+
+} else {
+
+if ($_POST['send_password'] == "1"){
+
+return true;
+
+} else {
+
+
+return false;
+
+}
+
+}
+
+}
+
+// Redefine user notification function
+
+if ( !function_exists('wp_new_user_notification') ) :
+/**
+ * Email login credentials to a newly-registered user.
+ *
+ * A new user registration notification is also sent to admin email.
+ *
+ * @since 2.0.0
+ * @since 4.3.0 The `$plaintext_pass` parameter was changed to `$notify`.
+ *
+ * @param int    $user_id User ID.
+ * @param string $notify  Whether admin and user should be notified ('both') or
+ *                        only the admin ('admin' or empty).
+ */
+function wp_new_user_notification( $user_id, $notify = '' ) {
+
+if (lh_email_registration_return_password_email()){
+	global $wpdb;
+	$user = get_userdata( $user_id );
+
+	// The blogname option is escaped with esc_html on the way into the database in sanitize_option
+	// we want to reverse this for the plain text arena of emails.
+	$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+
+	$message  = sprintf(__('New user registration on your site %s:'), $blogname) . "\r\n\r\n";
+	$message .= sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
+	$message .= sprintf(__('E-mail: %s'), $user->user_email) . "\r\n";
+
+	@wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), $blogname), $message);
+
+	if ( 'admin' === $notify || empty( $notify ) ) {
+		return;
+	}
+
+	// Generate something random for a password reset key.
+	$key = wp_generate_password( 20, false );
+
+	/** This action is documented in wp-login.php */
+	do_action( 'retrieve_password_key', $user->user_login, $key );
+
+	// Now insert the key, hashed, into the DB.
+	if ( empty( $wp_hasher ) ) {
+		require_once ABSPATH . WPINC . '/class-phpass.php';
+		$wp_hasher = new PasswordHash( 8, true );
+	}
+	$hashed = time() . ':' . $wp_hasher->HashPassword( $key );
+	$wpdb->update( $wpdb->users, array( 'user_activation_key' => $hashed ), array( 'user_login' => $user->user_login ) );
+
+	$message = sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
+	$message .= __('To set your password, visit the following address:') . "\r\n\r\n";
+	$message .= '<' . network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user->user_login), 'login') . ">\r\n\r\n";
+
+	$message .= wp_login_url() . "\r\n";
+
+	wp_mail($user->user_email, sprintf(__('[%s] Your username and password info'), $blogname), $message);
+}
+}
+endif;
 
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,21 +1,26 @@
-
 === LH Email Registration ===
 Contributors: shawfactor
 Donate link: http://lhero.org/plugins/lh-email-registration/
 Tags: email, backend, wp-admin, admin, registration
 Requires at least: 3.0.
-Tested up to: 4.2
+Tested up to: 4.3
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Streamlines user registration in the backend by removing redundant fields and replaces usernames with email addresses
+Streamlines user registration in the back end by allowing administrators to remove redundant fields and replaces usernames with email addresses
 
 == Description ==
 
-This plugin removes the username field from the add new user screen in the WordPress back end, instead the email address is used as the username. It also removed the website field (generally not required), as well as password fields (as passwords are auto generated). These changes make adding new users to WordPress a lot faster.
+This plugin allows the following you to configure the backend new user screen to incorporate the following features
 
-One word of caution is that this plugin replaces the username with the email address of the user when a user is saved in the database. meaning your users will no longer have a separate username. 
+* Optional use of email addresses only instead of username and email.
+* Optionally hide the new user url field
+* Optionally allow you to specify whether an email should be sent to each new user or not
+
+These changes make adding new users to WordPress a lot faster.
+
+One word of caution is that IF YOU USE EMAL ADDRESSES ONLly the plugin replaces the username with the email address of the user when a user is saved in the database. meaning your users will no longer have a separate username. 
 
 Check out [our documentation][docs] for more information.
 
@@ -28,6 +33,7 @@ All tickets for the project are being tracked on [GitHub][].
 
 1. Upload the entire `lh-email-registration` folder to the `/wp-content/plugins/` directory.
 2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Configure the setting via Settings->LH Email Registration
 3. Start adding user using the streamlined form via Users->Add New
 
 
@@ -35,4 +41,7 @@ All tickets for the project are being tracked on [GitHub][].
 
 **1.0 July 30, 2015**  
 Initial release.
+
+**2.0 July 30, 2015**  
+Complete overhaul for WordPress 4.3
 
